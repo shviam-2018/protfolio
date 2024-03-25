d295c400db7adcf70cd540f4885e3331e31a63b9
shviam-2018 <122033083+shviam-2018@users.noreply.github.com>
2024-03-25 18:57:28 +0100

    enhanced

--- emailslicer.py
+++ emailslicer.py
@@ -2,4 +2,9 @@ email = input("Enter email: ").strip()
 
 username, domain = email.split('@')
 
+if domain == "gmail.com":
+    domain = "Google Mail - default domain"
+else:
+    domain = str(domain) + " - business domain"
+
 print(f"Your username is {username} and domain is {domain}")
